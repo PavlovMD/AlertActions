67b8885acdbe12adcfdee063ee2d3452fb2c444b
Maksim <dev.superstarper@gmail.com>
2021-01-29 09:52:41 +0300

    Initial Commit

---
+++ AlertActions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlertActions
+//
+//  Created by Maksim Pavlov on 29.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
